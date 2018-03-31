ab6cb4493923e23ac909ac103283c164dc32217c
lvchunhui7 <lvchunhui7@126.com>
2018-03-31 10:13:49 +0800

    send_mail by django

--- index/views.py
+++ index/views.py
@@ -5,11 +5,14 @@ Created on 2018年3月28日
 @author: lch
 '''
 from django.http.response import HttpResponse, JsonResponse
-from email.mime.text import MIMEText
-import smtplib
+from django.core.mail import send_mail
 from django.views.generic.base import View
 from django.shortcuts import render
 from .models import MerchantApply
+import logging
+from django.conf import settings
+from django.core.mail.message import EmailMultiAlternatives
+logger = logging.getLogger('wafuli')
 class IndexView(View):
     def get(self, request):
         template = 'index.html'
@@ -41,31 +44,41 @@ class IndexView(View):
         if wangwang:
             content += '<p>联系人旺旺：%s</p>' % wangwang
         try:
-            send_email('5815397@qq.com', '商家报名', content)
-        except:
-            pass
+            from_email = settings.DEFAULT_FROM_EMAIL
+            # subject 主题 content 内容 to_addr 是一个列表，发送给哪些人
+            msg = EmailMultiAlternatives('商家报名', content, from_email, ['690501772@qq.com'])
+             
+            msg.content_subtype = "html"
+             
+            # 添加附件（可选）
+#             msg.attach_file('./twz.pdf')
+             
+            # 发送
+            msg.send()
+        except Exception as e:
+            logger.info("send email failed:" + str(e))
         return JsonResponse(res)
     
 
 
-def send_email(to_email, subject, content):
-    mail_host="smtp.126.com"  #设置服务器
-    mail_user="hunanjinyezi@126.com"    #用户名
-    mail_pass="jinyezi520"   #口令 
-    mail_postfix="126.com"  #发件箱的后缀
-    msg = MIMEText(content,_subtype='html',_charset='utf-8') #创建一个实例，这里设置为html格式邮件
-    msg['Subject'] = subject    #设置主题
-    msg['From'] = u"福利联盟<" + mail_user + ">"
-    msg['To'] = to_email
-    try:  
-        s = smtplib.SMTP()  
-        s.connect(mail_host)  #连接smtp服务器
-        s.login(mail_user,mail_pass)  #登陆服务器
-        res = s.sendmail(mail_user, [to_email,], msg.as_string())  #发送邮件
-        s.close()  
-        return 0
-    except Exception as e:
-        print('error')
-        print (e)
-        return 1
+# def send_email(to_email, subject, content):
+#     mail_host="smtp.126.com"  #设置服务器
+#     mail_user="hunanjinyezi@126.com"    #用户名
+#     mail_pass="jinyezi520"   #口令 
+#     mail_postfix="126.com"  #发件箱的后缀
+#     msg = MIMEText(content,_subtype='html',_charset='utf-8') #创建一个实例，这里设置为html格式邮件
+#     msg['Subject'] = subject    #设置主题
+#     msg['From'] = u"福利联盟<" + mail_user + ">"
+#     msg['To'] = to_email
+#     try:  
+#         s = smtplib.SMTP()  
+#         s.connect(mail_host)  #连接smtp服务器
+#         s.login(mail_user,mail_pass)  #登陆服务器
+#         res = s.sendmail(mail_user, [to_email,], msg.as_string())  #发送邮件
+#         s.close()  
+#         return 0
+#     except Exception as e:
+#         print('error')
+#         print (e)
+#         return 1
 
