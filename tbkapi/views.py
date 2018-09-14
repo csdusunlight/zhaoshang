dbf474adf2e1ae1d1b8097e6e4f86dbe85788e7b
lvchunhui7 <lvchunhui7@126.com>
2018-09-14 16:41:55 +0800

    domainname

--- tbkapi/views.py
+++ tbkapi/views.py
@@ -7,17 +7,17 @@ from django.http.response import JsonResponse
 
 def tpwdtoid(request):
     param = request.get_full_path().split('?')[1]
-    res = requests.get('https://api.open.myquan.cc/apiv1/tpwdtoid?'+param, verify=False)
+    res = requests.get('https://api.open.21ds.cn/apiv1/tpwdtoid?'+param, verify=False)
     return JsonResponse(res.json())
 def getitemgyurl(request):
     param = request.get_full_path().split('?')[1]
-    res = requests.get('https://api.open.myquan.cc/apiv1/getitemgyurl?'+param, verify=False)
+    res = requests.get('https://api.open.21ds.cn/apiv1/getitemgyurl?'+param, verify=False)
     return JsonResponse(res.json())
 def getitemgyurlbytpwd(request):
     param = request.get_full_path().split('?')[1]
-    res = requests.get('https://api.open.myquan.cc/apiv1/getitemgyurlbytpwd?'+param, verify=False)
+    res = requests.get('https://api.open.21ds.cn/apiv1/getitemgyurlbytpwd?'+param, verify=False)
     return JsonResponse(res.json())
 def gettkorder(request):
     param = request.get_full_path().split('?')[1]
-    res = requests.get('https://api.open.myquan.cc/apiv1/gettkorder?'+param, verify=False)
+    res = requests.get('https://api.open.21ds.cn/apiv1/gettkorder?'+param, verify=False)
     return JsonResponse(res.json())
